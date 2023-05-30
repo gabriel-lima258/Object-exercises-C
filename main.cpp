d2317996c3396079245997474e0a2d1cd9873585
Gabriel Lima <gabriel58221@gmail.com>
2023-05-30 17:32:17 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Cartesiano
+{
+private:
+    double x, y;
+public:
+    void entrada_dados();
+    void mostra_dados();
+};
+
+void Cartesiano::entrada_dados()
+{
+    cout << "Digite a coordenada x do plano: ";
+    cin >> x;
+    cout << "Digite a coordenada y do plano: ";
+    cin >> y;
+}
+
+void Cartesiano::mostra_dados()
+{
+    cout << "Coordenadas do sistema cartesiano: " << endl;
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+}
+
+int main() {
+
+    Cartesiano dc;
+
+    dc.entrada_dados();
+    dc.mostra_dados();
+
+    return 0;
+}
